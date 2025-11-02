2056d854ba9d016d9ab2f788c36c14c499d1310f
Mustapha Amine Kechaou <mustaphaamine.kechaou@gmail.com>
2025-11-02 11:34:58 +0100

    operation

--- Operation.java
+++ Operation.java
@@ -2,4 +2,10 @@ public class Operation {
         public static int produit(int x, int y){
                 return x*y;
         }
+  public static int addition(int x, int y){
+                return x+y;
+        }
+  public static int soustraction(int x, int y){
+                return x-y;
+        }
 }
